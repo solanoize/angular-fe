4fcbcff32a3deba3c8f4f328afe028f7b411fa51
Solanoize <9783318+solanoize@users.noreply.github.com>
2024-10-12 20:27:21 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './services/product.service';
+import { IProduct } from './interfaces/i-product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-fe';
+export class AppComponent implements OnInit {
+  products: IProduct[] = [];
+  product!: IProduct;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.onAllProduct();
+  }
+
+  onAllProduct() {
+    this.productService.all().subscribe((resp: IProduct[]) => {
+      this.products = resp;
+    });
+  }
 }
